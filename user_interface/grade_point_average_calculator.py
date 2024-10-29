b94451a71cff6f07217c25b993852dd2c0ae328c
Damien Altenburg <daltenburg@rrc.ca>
2024-10-29 15:47:37 -0500

    Update to complete the functionality of the app.

--- user_interface/grade_point_average_calculator.py
+++ user_interface/grade_point_average_calculator.py
@@ -9,6 +9,8 @@ class GradePointAverageCalculator(GPAWindow):
     A window that allows for GradePointAverage to be calculated.
     Inherited from GPAWindow which provides the gui design.
     """
+
+    new_gpa = Signal(str, float)
     
     def __init__(self, student_number: str, name: str):
         """
@@ -29,6 +31,8 @@ class GradePointAverageCalculator(GPAWindow):
         self.credit_edit_2.textChanged.connect(self.__enable_button)
         self.credit_edit_3.textChanged.connect(self.__enable_button)
 
+        self.calculate_button.clicked.connect(self.__on_calculate_clicked)
+
     @Slot()
     def __enable_button(self):
         """
@@ -46,5 +50,48 @@ class GradePointAverageCalculator(GPAWindow):
         Emits a signal with the student number and calculated gpa when complete.
         Formula: ((grade1 * cr_hours1) + (grade2 * cr_hours2) + etc) / sum(cr_hours#)
         """
+        line_edit_widgets = [self.credit_edit_1,
+                            self.credit_edit_2,
+                            self.credit_edit_3
+                            ]
 
+        credit_hours = []
 
+        for line_edit_widget in line_edit_widgets:
+            try:
+                credit_hour = float(line_edit_widget.text())
+
+                credit_hours.append(credit_hour)
+            except ValueError:
+                QMessageBox.information(self, 
+                                        "Credit Hours", 
+                                        "Credit Hours must be numeric.")
+
+                line_edit_widget.setFocus()
+
+                # Don't do the rest of the function when there is an error
+                return
+
+        grade_select_widgets = [self.grade_select_1,
+                                self.grade_select_2,
+                                self.grade_select_3
+                                ]
+        
+        grades = []
+
+        for grade_select_widget in grade_select_widgets:
+            # Look up the grade value
+            grades.append(self.GRADE_LOOKUP[grade_select_widget.currentText()])
+
+        total_grades = 0
+        total_credit_hours = 0
+
+        for grade, credit_hour in zip(grades, credit_hours):
+            total_grades += grade * credit_hour
+            total_credit_hours += credit_hour
+
+        grade_point_average = 0 if total_credit_hours == 0 else total_grades / total_credit_hours
+
+        self.grade_point_average_label.setText(f"{grade_point_average:.2f}")
+
+        self.new_gpa.emit(self.student_number_label.text(), grade_point_average)    
